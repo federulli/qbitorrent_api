051e9a87026c1c18750e2e871fb00fe580f356c5
federulli <fede.rulli@hotmail.com>
2018-08-18 13:07:36 -0300

    first commit

---
+++ qbittorrent_api/__init__.py
@@ -0,0 +1,28 @@
+import requests
+
+HOST = 'http://127.0.0.1:8080'
+
+
+def get_torrents(status='all'):
+    """
+    :param status: all, downloading, completed,
+        paused, active, inactive
+    :return:
+    """
+    r = requests.get(
+        '{}/api/v2/torrents/info?filter={}'.format(HOST, status)
+    )
+    r.raise_for_status()
+    return r.json()
+
+
+def post_torrent(magnet, download_path):
+    r = requests.post(
+            '{}/api/v2/torrents/add'.format(HOST),
+            data=dict(
+                urls=magnet,
+                savepath=download_path
+            )
+    )
+    r.raise_for_status()
+
---
+++ setup.py
@@ -0,0 +1,12 @@
+import os
+from setuptools import setup, find_packages
+
+
+setup(
+    name="qbittorrent_api",
+    version="1.0.0",
+    author="Federico Rulli",
+    author_email="fede.rulli@gmail.com",
+    description="qBittorrent Api",
+    packages=find_packages()
+)
