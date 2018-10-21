faacf0fe25af5445a1e26a7bb806a0a19f106817
federulli <fede.rulli@hotmail.com>
2018-10-21 10:14:57 -0300

    add delete torrents

--- qbittorrent_api/__init__.py
+++ qbittorrent_api/__init__.py
@@ -3,14 +3,14 @@ import requests
 HOST = 'http://127.0.0.1:8080'
 
 
-def get_torrents(status='all'):
+def get_torrents():
     """
     :param status: all, downloading, completed,
         paused, active, inactive
     :return:
     """
     r = requests.get(
-        '{}/api/v2/torrents/info?filter={}'.format(HOST, status)
+        '{}/query/torrents'.format(HOST)
     )
     r.raise_for_status()
     return r.json()
@@ -24,4 +24,14 @@ def post_torrent(magnet, download_path):
                 savepath=download_path
             )
     )
+    r.raise_for_status()
+
+
+def delete_completed_torrent():
+    torrents = get_torrents()
+    hashes = "|".join(
+        torrent["hash"]
+        for torrent in torrents if torrent['state'] in ["uploading"]
+    )
+    r = requests.post("{}/command/delete".format(HOST), data=dict(hashes=hashes))
     r.raise_for_status()
